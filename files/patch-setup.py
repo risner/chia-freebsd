a98c9e71e92160babd1147c692f3fa7107d64aa4
risner <risner@users.noreply.github.com>
2021-05-30 01:14:20 +0000

    Create patch-setup.py

---
+++ files/patch-setup.py
@@ -0,0 +1,11 @@
+--- setup.py.orig	2021-05-28 15:07:56.168639000 -0400
++++ setup.py	2021-05-28 15:08:13.642924000 -0400
+@@ -13,7 +13,7 @@
+     "bitstring==3.1.7",  # Binary data management library
+     "colorlog==5.0.1",  # Adds color to logs
+     "concurrent-log-handler==0.9.19",  # Concurrently log and rotate logs
+-    "cryptography==3.4.7",  # Python cryptography library for TLS - keyring conflict
++    "cryptography==3.3.2",  # Python cryptography library for TLS - keyring conflict
+     "keyring==23.0.1",  # Store keys in MacOS Keychain, Windows Credential Locker
+     "keyrings.cryptfile==1.3.4",  # Secure storage for keys on Linux (Will be replaced)
+     #  "keyrings.cryptfile==1.3.8",  # Secure storage for keys on Linux (Will be replaced)
